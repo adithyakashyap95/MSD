757235669e872d80df3e2041fa7487085bfa9f22
Adithya Rajagopal <117482223+adithyakashyap95@users.noreply.github.com>
2022-11-05 00:33:34 -0700

    Add files via upload

---
+++ Cache.sv
@@ -0,0 +1,11 @@
+
+module cache #(
+	parameter	WIDTH = 1
+)(
+	input logic 	clk,
+	input logic 	rst
+);
+
+// Black box
+
+endmodule
---
+++ Cache_TB.sv
@@ -0,0 +1,51 @@
+
+module cache_TB;
+
+logic  clk;
+logic  rst;
+int    event_open;
+string line;
+
+cache #(
+	.WIDTH		(1)
+) DUT (
+	.clk		(clk),
+	.rst		(rst)
+);
+
+initial 
+begin
+	clk = 0;
+	rst = 1;
+
+	event_open = $fopen("./Hex_file/event.txt","r");
+
+	if($test$plusargs ("silent"))
+		$display("silent Mode is  used");
+	else if($test$plusargs ("Silent"))
+		$display("silent Mode is  used");
+	else if($test$plusargs ("s"))
+		$display("silent Mode is  used");
+	else if($test$plusargs ("S"))
+		$display("silent Mode is  used");
+	
+	if($test$plusargs ("normal"))
+		$display("normal Mode is  used");
+	else if($test$plusargs ("Normal"))
+		$display("normal Mode is  used");
+	else if($test$plusargs ("n"))
+		$display("normal Mode is  used");
+	else if($test$plusargs ("N"))
+		$display("normal Mode is  used");
+
+$display(" ");
+
+	for (int i=0;i<6;i=i+1)
+	begin
+		$fgets(line,event_open);
+		$display("%s ",line);
+	end
+
+end
+
+endmodule

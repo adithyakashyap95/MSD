8e9c214352454158ca710f76c0a0fc7a52a4542a
Adithya Rajagopal <117482223+adithyakashyap95@users.noreply.github.com>
2022-11-05 11:37:41 -0700

    After review with TA, changes turned in

--- Cache_TB.sv
+++ Cache_TB.sv
@@ -5,6 +5,7 @@ logic  clk;
 logic  rst;
 int    event_open;
 string line;
+string filename;
 
 cache #(
 	.WIDTH		(1)
@@ -18,7 +19,13 @@ begin
 	clk = 0;
 	rst = 1;
 
-	event_open = $fopen("./event.txt","r");
+	$value$plusargs("FILENAME=%s",filename);
+	event_open = $fopen(filename,"r");
+
+	if(event_open)
+		$display("trace file is opened");
+	else
+		$display("trace file cannot be opened");
 
 	if($test$plusargs ("silent"))
 		$display("silent Mode is  used");
@@ -27,6 +34,8 @@ begin
 	else if($test$plusargs ("s"))
 		$display("silent Mode is  used");
 	else if($test$plusargs ("S"))
+		$display("silent Mode is  used");
+	else if($test$plusargs ("SILENT"))
 		$display("silent Mode is  used");
 	
 	if($test$plusargs ("normal"))
@@ -37,7 +46,8 @@ begin
 		$display("normal Mode is  used");
 	else if($test$plusargs ("N"))
 		$display("normal Mode is  used");
-
+	else if($test$plusargs ("NORMAL"))
+		$display("normal Mode is  used");
 $display(" ");
 $display("n address \n");
 	for (int i=0;i<6;i=i+1)

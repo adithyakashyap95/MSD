f289b1bbcb317a7e2eeeab8967eb884abe684b71
Adithya Rajagopal <adithyakashyap95@gmail.com>
2022-11-22 17:33:11 -0800

    Update with address logic

--- Cache.sv
+++ Cache.sv
@@ -2,26 +2,38 @@
 module cache #(
 // All are taken as local parm 
 )(
-	input logic 	clk,
-	input logic 	rstb
+	input logic 	   clk,
+	input logic 	   rstb,
+	input logic [31:0] address,
+	input logic  [3:0] n
 );
 
 `include "Cache_struct.sv"
 sets_nway_t [(NUM_OF_SETS-1):0] sets;
 sets_nway_t [(NUM_OF_SETS-1):0] sets_nxt;
+logic                           update_sets;
+logic [(TAG-1):0]               tag_in;       
+logic [(INDEX-1):0]             index_in;       
+logic [(BYTE-1):0]              byte_offset_in;
+logic [(WAYS_REP-1):0]          ways_in;
 
-// Creating flops for the whole cache
-always_ff@(posedge clk or negedge rstb)
-begin
-	if(rstb==0)
-	begin
-		sets <= 0;
-	end
-	else
-	begin
-		sets <= sets_nxt;
-	end
-end
+// decoding n
+/*
+0 read request from L1 data cache
+1 write request from L1 data cache
+2 read request from L1 instruction cache
+3 snooped invalidate command
+4 snooped read request
+5 snooped write request
+6 snooped read with intent to modify request
+8 clear the cache and reset all state
+9 print contents and state of each valid cache line (doesn?t end simulation!)
+*/
+
+// decode address
+assign tag_in         = address[31:(INDEX-1)]; 
+assign index_in       = address[(INDEX-1):(BYTE-1)];
+assign byte_offset_in = address[(BYTE-1):0];
 
 // Module to interface with each bit of LRU
 /* Functions to implement
@@ -36,5 +48,31 @@ end
 	4. DATA transfer based on Hit and Miss.
 	5. Monitor the snoop and give iut snoop result.
 */
+
+// CORE LOGIC starts from here
+assign update_sets = 1'b0;  // FIXME : Build the combi logic for updating the sets based on LRU and MESI
+
+// Creating flops for the whole cache
+always_ff@(posedge clk or negedge rstb)
+begin
+	if(rstb==0)
+	begin
+		sets <= 0;
+	end
+	else if(update_sets)
+	begin
+		sets <= sets_nxt;
+	end
+	else
+	begin
+		sets <= sets;
+	end
+end
+
+always_comb
+begin
+	sets_nxt[index_in].line[ways_in].tag = tag_in; 
+	sets_nxt[index_in].line[ways_in].byte_select = byte_offset_in; 
+end
 
 endmodule
--- Cache_defines.sv
+++ Cache_defines.sv
@@ -1,9 +1,31 @@
 
-localparam WAYS         = 8;                                // 8 WAY ASSOCIATIVE
-localparam CAPACITY     = 16000000;                         // 16MB capacity
-localparam LINE_SIZE    = 64;                               // LINE size
-localparam INDEX        = (CAPACITY/(LINE_SIZE/WAYS));      // 16MB/64/8 = 2^(15) 
-localparam BYTE         = $clog2(64);                       // log(64) where 64B is byte line 
-localparam TAG          = (32 - INDEX - BYTE);              // 32 is because of unsigned integer  
-localparam NUM_OF_LINES = (2**(INDEX))*WAYS;                // 2^(15+8)
-localparam NUM_OF_SETS  = (2**(INDEX));                     // 2^(15)
+localparam WAYS           = 8;                                // 8 WAY ASSOCIATIVE
+localparam WAYS_REP       = $clog2(WAYS);                     // log(8) WAY ASSOCIATIVE to represent
+localparam CAPACITY       = 16000000;                         // 16MB capacity
+localparam LINE_SIZE      = 64;                               // LINE size
+localparam INDEX          = $clog2((CAPACITY/(LINE_SIZE/WAYS))); // 16MB/64/8 = 2^(15) 
+localparam BYTE           = $clog2(64);                       // log(64) where 64B is byte line 
+localparam TAG            = (32 - INDEX - BYTE);              // 32 is because of unsigned integer  
+localparam NUM_OF_LINES   = (2**(INDEX))*WAYS;                // 2^(15+8)
+localparam NUM_OF_SETS    = (2**(INDEX));                     // 2^(15)
+
+/* Bus Operation types */
+localparam READ           = 1;                                /* Bus Read */
+localparam WRITE          = 2;                                /* Bus Write */
+localparam INVALIDATE     = 3;                                /* Bus Invalidate */
+localparam RWIM           = 4;                                /* Bus Read With Intent to Modify */
+
+/* Snoop Result types */
+localparam NOHIT          = 0;                                /* No hit */
+localparam HIT            = 1;                                /* Hit */
+localparam HITM           = 2;                                /* Hit to modified line */
+
+/* L2 to L1 message types */
+localparam GETLINE        = 1;                                /* Request data for modified line in L1 */
+localparam SENDLINE       = 2;                                /* Send requested cache line to L1 */
+localparam INVALIDATELINE = 3;                                /* Invalidate a line in L1 */
+localparam EVICTLINE      = 4;                                /* Evict a line from L1 */
+
+// this is when L2's replacement policy causes eviction of a line that
+// may be present in L1. It could be done by a combination of GETLINE
+// (if the line is potentially modified in L1) and INVALIDATELINE.
